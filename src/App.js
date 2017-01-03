bacfe3ffa3fc461fb749b79395c803c3f15a309a
Mahdi <mahdi@LearniOSFreecoms-Mac.local>
2017-01-02 23:09:18 -0800

    Boilerplate setup plus firebase for authentication

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import {View, Text} from "react-native";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import reducers from "./reducers";
+import firebase from "firebase";
+
+class App extends Component {
+
+	componentWillMount() {
+		const config = {
+			apiKey: "AIzaSyC5yHf5PJo-XGLkuJAfiDE-RrHWNucE0Rc",
+			authDomain: "manager-45fb6.firebaseapp.com",
+			databaseURL: "https://manager-45fb6.firebaseio.com",
+			storageBucket: "manager-45fb6.appspot.com",
+			messagingSenderId: "650318683056"
+		};
+
+		firebase.initializeApp(config);
+
+	}
+
+	render() {
+		return (
+			<Provider store={createStore(reducers)}>
+				<View>
+					<Text>
+						Hello!
+					</Text>
+				</View>
+			</Provider>
+		)
+	}
+}
+
+export default App;
