3a75fa8b120824a24ae6b221183fccf8f17c1aa2
Mahdi <mahdi@LearniOSFreecoms-Mac.local>
2017-01-18 00:32:53 -0800

    added spinner and error handling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
-import {View, Text} from "react-native";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import ReduxThunk from "redux-thunk";
 import reducers from "./reducers";
 import firebase from "firebase";
 import LoginForm from "./components/LoginForm.js";
@@ -22,14 +22,11 @@ class App extends Component {
 	}
 
 	render() {
+		const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
 		return (
-			<Provider store={createStore(reducers)}>
-				<View>
-					<Text>
-						Hello!
-					</Text>
+			<Provider store={store}>
 					<LoginForm />
-				</View>
 			</Provider>
 		)
 	}
